a23654f25ef9d75673a8ce39e53db448e9832bc3
hyeonorof <hyeonorof@gmail.com>
2022-07-06 11:11:45 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week03_spring_project/domain/MemoRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.week03_spring_project.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    //List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end);
+
+}
---
+++ src/test/java/com/sparta/week03_spring_project/Week03SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week03_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week03SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
